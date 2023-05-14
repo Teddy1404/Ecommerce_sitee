f982e9b9204101d77d73822936e2a75812dd60d4
Teddy1404 <aryansinghchandel1404@gmail.com>
2023-05-14 21:08:43 +0530

    backend updated

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createProductController, deleteProductController, getProductController, getSingleProductController, productPhotoController, updateProductController} from "../controllers/productController.js"
+import {createProductController, deleteProductController, getProductController, getSingleProductController, productCountController, productFiltersController, productListController, productPhotoController, searchProductController, updateProductController} from "../controllers/productController.js"
 import {isAdmin , requireSignIn} from "../middlewares/authMiddleware.js";
 import formidable from 'express-formidable'
 const router = express.Router();
@@ -29,4 +29,14 @@ router.get("/product-photo/:pid", productPhotoController);
 //delete rproduct
 router.delete("/delete-product/:pid", deleteProductController);
 
+
+router.post('/product-filters', productFiltersController);
+//product count
+router.get("/product-count", productCountController);
+
+//product per page
+router.get("/product-list/:page", productListController);
+
+//search product
+router.get("/search/:keyword", searchProductController);
 export default router;
